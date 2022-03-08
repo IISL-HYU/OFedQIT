8f32bdcdf0a68b03e4740cb72605317f8150bb0d
Dohyeok Kwon <kwon1585@gmail.com>
2022-03-08 18:27:11 +0900

    22.03.08 18:27

--- main_regression.py
+++ main_regression.py
@@ -13,7 +13,7 @@ import math
 N = 100
 L = 1
 L1 = 2
-L2 = 3
+L2 = 2
 
 prob = 1.0
 prob2 = 0.5
@@ -41,25 +41,24 @@ input_size = len(X)
 X_t_train = [list(i) for i in zip(*X)]
 X_t_train = np.array(X_t_train).reshape(T, input_size, 1)
 
-reuse = 6
+reuse = 1
 data = "Conductivity"
 b = 1
 
 # models with prob=1.0
 all_models, central_server = mg.model_generation_regression(N, input_size)
-q_all_models, q_central_server = mg.model_generation_regression(N, input_size)
+# q_all_models, q_central_server = mg.model_generation_regression(N, input_size)
 q2_all_models, q2_central_server = mg.model_generation_regression(N, input_size)
 
 # models with prob=0.5
-p2_all_models, p2_central_server = mg.model_generation_regression(N, input_size)
-q_p2_all_models, q_p2_central_server = mg.model_generation_regression(N, input_size)
-q2_p2_all_models, q2_p2_central_server = mg.model_generation_regression(N, input_size)
+# p2_all_models, p2_central_server = mg.model_generation_regression(N, input_size)
+# q_p2_all_models, q_p2_central_server = mg.model_generation_regression(N, input_size)
+# q2_p2_all_models, q2_p2_central_server = mg.model_generation_regression(N, input_size)
 
 # models with prob=0.1
 p3_all_models, p3_central_server = mg.model_generation_regression(N, input_size)
-q_p3_all_models, q_p3_central_server = mg.model_generation_regression(N, input_size)
+# q_p3_all_models, q_p3_central_server = mg.model_generation_regression(N, input_size)
 q2_p3_all_models, q2_p3_central_server = mg.model_generation_regression(N, input_size)
-
 
 loss_list = [1]
 q_loss_list = [1]
@@ -94,70 +93,73 @@ for iter in range(reuse):
     results = all_models.Lpfed_avg(x, y, central_server, prob, L, i)
     loss_list.append(results)
     # OFedQIT model I (L=1)
-    q_results = q_all_models.Lpqfed_avg(x, y, q_central_server, prob, L1, i, b)
-    q_loss_list.append(q_results)
+    # q_results = q_all_models.Lpqfed_avg(x, y, q_central_server, prob, L1, i, b)
+    # q_loss_list.append(q_results)
     # OFedQIT model II (L=10)
     q2_results = q2_all_models.Lpqfed_avg(x, y, q2_central_server, prob, L2, i, b)
     q2_loss_list.append(q2_results)
     
-    # Benchmarking model(p=0.5)
-    p2_results = p2_all_models.Lpfed_avg(x, y, p2_central_server, prob2, L, i)
-    p2_loss_list.append(p2_results)
-    # OFedQIT model I (L=1)
-    q_p2_results = q_p2_all_models.Lpqfed_avg(x, y , q_p2_central_server, prob2, L1, i, b)
-    q_p2_loss_list.append(q_p2_results)
-    # # OFedQIT model II (L=3)
-    q2_p2_results = q2_p2_all_models.Lpqfed_avg(x, y, q2_p2_central_server, prob2, L2, i, b)
-    q2_p2_loss_list.append(q2_p2_results)
+    # # Benchmarking model(p=0.5)
+    # p2_results = p2_all_models.Lpfed_avg(x, y, p2_central_server, prob2, L, i)
+    # p2_loss_list.append(p2_results)
+    # # OFedQIT model I (L=1)
+    # q_p2_results = q_p2_all_models.Lpqfed_avg(x, y , q_p2_central_server, prob2, L1, i, b)
+    # q_p2_loss_list.append(q_p2_results)
+    # # # OFedQIT model II (L=3)
+    # q2_p2_results = q2_p2_all_models.Lpqfed_avg(x, y, q2_p2_central_server, prob2, L2, i, b)
+    # q2_p2_loss_list.append(q2_p2_results)
     
     # Benchmarking model(p=0.1)
     p3_results = p3_all_models.Lpfed_avg(x, y, p3_central_server, prob3, L, i)
     p3_loss_list.append(p3_results)
     # OFedQIT model I (L=1)
-    q_p3_results = q_p3_all_models.Lpqfed_avg(x, y , q_p3_central_server, prob3, L1, i, b)
-    q_p3_loss_list.append(q_p3_results)
-    # # OFedQIT model II (L=3)
+    # q_p3_results = q_p3_all_models.Lpqfed_avg(x, y , q_p3_central_server, prob3, L1, i, b)
+    # q_p3_loss_list.append(q_p3_results)
+    # OFedQIT model II (L=3)
     q2_p3_results = q2_p3_all_models.Lpqfed_avg(x, y, q2_p3_central_server, prob3, L2, i, b)
     q2_p3_loss_list.append(q2_p3_results)
 
     if(i % 20 == 0):
       print("iteration : ", iter, ", i : ", i)
       print("loss : %.7f " %(results))
-      print("[Q]loss : %.7f " %(q_results))
+      # print("[Q]loss : %.7f " %(q_results))
       print("[Q2]loss : %.7f " %(q2_results))
       
-      print("loss : %.7f " %(p2_results))
-      print("[Q]loss : %.7f " %(q_p2_results))
-      print("[Q2]loss : %.7f " %(q2_p2_results))
+      # print("loss : %.7f " %(p2_results))
+      # print("[Q]loss : %.7f " %(q_p2_results))
+      # print("[Q2]loss : %.7f " %(q2_p2_results))
       
-      print("loss : %.7f " %(results))
-      print("[Q]loss : %.7f " %(q_p3_results))
+      print("loss : %.7f " %(p3_results))
+      # print("[Q]loss : %.7f " %(q_p3_results))
       print("[Q2]loss : %.7f " %(q2_p3_results))
     
-with open("./Regression_mse/OFedAvg_{data}_L{L}_p{prob}.pkl","wb") as f:
+    
+with open(f"./Regression_mse/OFedAvg_{data}_L{L}_p{prob}.pkl","wb") as f:
     pickle.dump(loss_list, f)
     
-with open("./Regression_mse/OFedQIT_{data}_L{L1}_s1_b{b}_p{prob}.pkl","wb") as f:
-    pickle.dump(q_loss_list, f)
+# with open(f"./Regression_mse/OFedQIT_{data}_L{L1}_s1_b{b}_p{prob}.pkl","wb") as f:
+#     pickle.dump(q_loss_list, f)
 
-with open("./Regression_mse/OFedQIT_{data}_L{L2}_s1_b{b}_p{prob}.pkl","wb") as f:
+with open(f"./Regression_mse/OFedQIT_{data}_L{L2}_s1_b{b}_p{prob}.pkl","wb") as f:
     pickle.dump(q2_loss_list, f)
     
-with open("./Regression_mse/OFedAvg_{data}_L{L}_p{prob2}.pkl","wb") as f:
-    pickle.dump(p2_loss_list, f)
     
-with open("./Regression_mse/OFedQIT_{data}_L{L1}_s1_b{b}_p{prob2}.pkl","wb") as f:
-    pickle.dump(q_p2_loss_list, f)
+# with open(f"./Regression_mse/OFedAvg_{data}_L{L}_p{prob2}.pkl","wb") as f:
+#     pickle.dump(p2_loss_list, f)
+    
+# with open(f"./Regression_mse/OFedQIT_{data}_L{L1}_s1_b{b}_p{prob2}.pkl","wb") as f:
+#     pickle.dump(q_p2_loss_list, f)
 
-with open("./Regression_mse/OFedQIT_{data}_L{L2}_s1_b{b}_p{prob2}.pkl","wb") as f:
-    pickle.dump(q2_p2_loss_list, f)
+# with open(f"./Regression_mse/OFedQIT_{data}_L{L2}_s1_b{b}_p{prob2}.pkl","wb") as f:
+#     pickle.dump(q2_p2_loss_list, f)
     
-with open("./Regression_mse/OFedAvg_{data}_L{L}_p{prob3}.pkl","wb") as f:
+    
+with open(f"./Regression_mse/OFedAvg_{data}_L{L}_p{prob3}.pkl","wb") as f:
     pickle.dump(p3_loss_list, f)
     
-with open("./Regression_mse/OFedQIT_{data}_L{L1}_s1_b{b}_p{prob3}.pkl","wb") as f:
-    pickle.dump(q_p3_loss_list, f)
+# with open(f"./Regression_mse/OFedQIT_{data}_L{L1}_s1_b{b}_p{prob3}.pkl","wb") as f:
+#     pickle.dump(q_p3_loss_list, f)
 
-with open("./Regression_mse/OFedQIT_{data}_L{L2}_s1_b{b}_p{prob3}.pkl","wb") as f:
+with open(f"./Regression_mse/OFedQIT_{data}_L{L2}_s1_b{b}_p{prob3}.pkl","wb") as f:
     pickle.dump(q2_p3_loss_list, f)
     
